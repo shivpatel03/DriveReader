f80cc0ad4525adad7022118c420e560344e315c7
Shiv Patel <da.shiv.patel@gmail.com>
2025-07-09 08:22:25 -0400

    Updated query to Drive API to include all file types

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ async function main() {
   // authorize the app
   const auth = await authorize();
 
+  // create hashset of file types that we can process (skip otherwise)
+  const fileTypes = new Set(['application/pdf', 'application/vnd.google-apps.document', 'application/vnd.google-apps.spreadsheet', 'application/vnd.google-apps.presentation', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'text/plain']);
+
   // get drive api client
   const drive = google.drive({ version: 'v3', auth });
 
   // get files in the drive
   const res = await drive.files.list({
-    q: "mimeType='application/vnd.google-apps.document'",
+    pageSize: 100,
     fields: 'nextPageToken, files(id, name, webViewLink, mimeType, kind)'
   })
   const files = res.data.files;
@@ -19,7 +22,11 @@ async function main() {
   // process files (convert to text)
   if (files.length !== 0) {
     files.forEach((file) => {
-      extractText(drive, file.id)
+      if (fileTypes.has(file.mimeType)) {
+        extractText(drive, file.id)
+      } else {
+        console.log(`❌ Skipping file ${file.name} because it is not a supported file type`);
+      }
     })
   } else {
     console.log('❌ No Files Found');
